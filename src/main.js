c933997cc9d4a3b72e2a954b1eb9dae25dedf84b
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-01-29 18:01:36 +0100

    add font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import api from './plugins/api'
 import piniaPersist from 'pinia-plugin-persist'
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
 
 import App from './App.vue'
 import router from './router'
@@ -10,10 +12,13 @@ const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPersist)
 
+// Icons
+library.add(faUser);
+
+app.use(api, {
+  baseUrl: import.meta.env.VITE_API_BASE_URL
+})
 app.use(pinia)
 app.use(router)
-app.use(api, {
-    baseUrl: import.meta.env.VITE_API_BASE_URL
-})
 
 app.mount('#app')
