040274526a24e30d7d32935a0f69a979e9b8f4d4
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-01-29 17:22:17 +0100

    installation de pinia persist

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import api from './plugins/api'
+import piniaPersist from 'pinia-plugin-persist'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPersist)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(api, {
     baseUrl: import.meta.env.VITE_API_BASE_URL,
