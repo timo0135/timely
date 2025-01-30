09cde5d3b3b935cba6c26c9ad7ee4a90548e9040
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-01-30 14:57:18 +0100

    store mise à jour et persistant

--- src/stores/authentification.js
+++ src/stores/authentification.js
@@ -1,69 +1,69 @@
-import axios from "axios"
-import { defineStore } from "pinia"
-import { ref, reactive } from "vue"
+import { defineStore } from 'pinia';
+import axios from 'axios';
 
-export const useAuthStore = defineStore('auth', () => {
-    const user = reactive({
-        id: null,
-        apikey: null,
-        email: null,
-        name: null
-    })
-    const errors = ref(null)
-
-    const isAuthenticated = ref(false)
-
-    const login = (apikey) => {
-        axios.get('https://timely.edu.netlor.fr/api/profile', {
-            headers: {
-                Authorization: `key=${apikey}`,
-                "Content-Type": "application/json"
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        user: {
+            id: null,
+            apikey: null,
+            email: null,
+            name: null
+        },
+        isAuthenticated: false,
+        errors: null
+    }),
+    actions: {
+        login(key) {
+            axios.get('https://timely.edu.netlor.fr/api/profile',{
+                headers: {
+                    Authorization: `key=${key}`,
+                    "Content-Type": "application/json"
+                }
+            }).then(response => {
+                this.user.id = response.data.id;
+                this.user.apikey = key;
+                this.user.email = response.data.email;
+                this.user.name = response.data.name;
+                this.isAuthenticated = true;
+            }).catch(error => {
+                this.errors = error;
+                console.error(error);
+            });
+        },
+        register(email, name) {
+            axios.post('https://timely.edu.netlor.fr/api/apikeys', {
+                email: email,
+                name: name
+            }, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(response => {
+                this.user.id = response.data.id;
+                this.user.apikey = response.data.key;
+                this.user.email = response.data.email;
+                this.user.name = response.data.name;
+                this.isAuthenticated = true;
+            }).catch(error => {
+                this.errors = error;
+                console.error(error);
+            });
+        },
+        logout() {
+            this.user.id = null;
+            this.user.apikey = null;
+            this.user.email = null;
+            this.user.name = null;
+            this.isAuthenticated = false;
+        }
+    },
+    persist: {
+        enabled: true,
+        strategies: [
+            {
+                key: 'auth',
+                storage: localStorage
             }
-        }).then(response => {
-            user.id = response.data.id
-            user.apikey = apikey
-            user.email = response.data.email
-            user.name = response.data.name
-            isAuthenticated.value = true
-        }).catch((error => {
-            errors.value = error
-            console.error(error);
-        }))
+        ]
     }
-
-    const register = (email, name) => {
-        axios.post('https://timely.edu.netlor.fr/api/apikeys',{
-            email: email,
-            name: name
-        }, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(response => {
-            user.id = response.data.id
-            user.apikey = response.data.key
-            user.email = response.data.email
-            user.name = response.data.name
-            isAuthenticated.value = true
-        }).catch((error => {
-            errors.value = error
-            console.error(error);
-        }))
-    }
-
-    const logout = () => {
-        user.id = null
-        user.apikey = null
-        user.email = null
-        user.name = null
-        isAuthenticated.value = false
-    }
-
-    return {
-        user,
-        isAuthenticated,
-        register,
-        login,
-        logout
-    }
-})
+});
