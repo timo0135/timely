25a4672b1f922ff389385721eb7196dc6e4107ac
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-01-31 21:55:24 +0100

    installation de vuetify

--- src/main.js
+++ src/main.js
@@ -1,24 +1,49 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
-import api from './plugins/api'
-import piniaPersist from 'pinia-plugin-persist'
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import api from './plugins/api';
+import piniaPersist from 'pinia-plugin-persist';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faEye, faEyeSlash, faGear, faPencilAlt, faUser} from '@fortawesome/free-solid-svg-icons';
 
-import App from './App.vue'
-import router from './router'
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
-const app = createApp(App)
-const pinia = createPinia()
-pinia.use(piniaPersist)
+import App from './App.vue';
+import router from './router';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPersist);
 
 // Icons
 library.add(faUser);
+library.add(faPencilAlt);
+library.add(faEye);
+library.add(faEyeSlash);
+library.add(faGear);
 
 app.use(api, {
-  baseUrl: import.meta.env.VITE_API_BASE_URL
-})
-app.use(pinia)
-app.use(router)
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
+});
+app.use(vuetify);
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
