3cc5d3c43843b9c390dbe86351cff5fade0bb61e
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-01-31 21:57:14 +0100

    Création d'un composant header pour les settings

---
+++ src/components/settings/SettingsHeaderComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="text-h4">Settings <font-awesome-icon :icon="['fas', 'gear']" /></h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn  to="/settings/profile" class="mr-4">Mon Profil</v-btn>
+        <v-btn  to="/settings/projects" class="mr-4">Mes Projets</v-btn>
+        <v-btn  to="/settings/activities">Mes Activités</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.v-btn {
+  text-transform: none;
+}
+</style>
