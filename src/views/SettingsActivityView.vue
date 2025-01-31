647dd612e7701f5ad88f4e2430195421a813e195
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-01-31 22:00:15 +0100

    Vue pour les activités (pas encore commencé)

---
+++ src/views/SettingsActivityView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SettingsHeaderComponent from '@/components/settings/SettingsHeaderComponent.vue';
+</script>
+
+<template>
+    <SettingsHeaderComponent />
+    <h2>Activities</h2>
+    <p>Here are your activities</p>
+
+</template>
+
+<style scoped>
+</style>
