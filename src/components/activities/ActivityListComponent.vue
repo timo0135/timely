b0b593cb1b9ab3ce50b1ac6535adca33f8b3a0f6
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-02-01 12:34:03 +0100

    Création de la vue avec la liste des activités

---
+++ src/components/activities/ActivityListComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps } from 'vue';
+// import { VList, VListItem, VListItemTitle } from 'vuetify/components';
+
+defineProps({
+  activities: Array,
+});
+
+function getActivityClass(activity) {
+  return activity.is_enabled === 0 ? 'disabled-activity' : '';
+}
+</script>
+
+<template>
+  <v-list-item
+    v-for="activity in activities"
+    :key="activity.id"
+    :class="getActivityClass(activity)"
+  >
+    <v-list-item-title>{{ activity.name }}</v-list-item-title>
+  </v-list-item>
+</template>
+
+<style scoped>
+
+.v-list-item {
+  cursor: pointer;
+  border-bottom: black 1px solid;
+}
+
+.v-list-item:hover {
+  background-color: #f0f0f0;
+
+}
+
+.disabled-activity {
+  opacity: 0.5;
+}
+</style>
--- src/views/SettingsActivityView.vue
+++ src/views/SettingsActivityView.vue
@@ -1,12 +1,77 @@
 <script setup>
 import SettingsHeaderComponent from '@/components/settings/SettingsHeaderComponent.vue';
+import ActivityListComponent from "@/components/activities/ActivityListComponent.vue";
+import { getCurrentInstance, onMounted, ref, watch, computed } from "vue";
+import { VCol, VContainer, VRow, VTextField, VCard, VCardTitle, VCardText, VPagination } from "vuetify/components";
+
+const activities = ref([]);
+const search = ref('');
+const page = ref(1);
+const itemsPerPage = ref(5);
+
+const api = getCurrentInstance().appContext.config.globalProperties.$api();
+
+const fetchProjects = (searchQuery) => {
+  api.get('/api/activities', {
+    params: {
+      keywords: searchQuery,
+    },
+  })
+    .then((response) => {
+      activities.value = response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+onMounted(() => {
+  fetchProjects(search.value);
+});
+
+watch(search, (newSearch) => {
+  fetchProjects(newSearch);
+});
+
+const paginatedActivities = computed(() => {
+  const start = (page.value - 1) * itemsPerPage.value;
+  const end = start + itemsPerPage.value;
+
+  return activities.value.slice(start, end);
+});
+
+const totalActivities = computed(() => activities.value.length);
+const numberOfPages = computed(() => Math.ceil(totalActivities.value / itemsPerPage.value));
 </script>
 
 <template>
+  <v-container>
     <SettingsHeaderComponent />
-    <h2>Activities</h2>
-    <p>Here are your activities</p>
-
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            <h2>Activités</h2>
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="search"
+              label="Search activities"
+              variant="outlined"
+              clearable
+            />
+            <v-list>
+              <ActivityListComponent :activities="paginatedActivities" />
+            </v-list>
+            <v-pagination
+              v-model="page"
+              :length="numberOfPages"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
